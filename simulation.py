7989241d7a8e6341144741a74c3add64dd8f1280
Burak Balaban <brk.blbn@gmail.com>
2020-01-29 00:50:21 +0300

    Decomposition for linear DGP added.

--- simulation.py
+++ simulation.py
@@ -242,3 +242,4 @@ if __name__ == '__main__':
     mcs.bar(title=f"RSS for linear DGP",
             filePath=f"plots/forest_vs_ols_linearDGP",
             logScale=True)
+    mcs.bvplot(filePath="plots/bias_var_linearDGP")
